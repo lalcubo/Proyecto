e2ba645d8aa30346e112010a22c7480e1db5dea2
Juan Ramirez <jramirezg@unet.edu.ve>
2012-11-06 17:22:28 -0430

    metodos y atributos
    Creacion de metodos y atributos de la Clase Obra

--- src/practica/Obra.java
+++ src/practica/Obra.java
@@ -9,5 +9,41 @@ package practica;
  * @author Juan
  */
 public class Obra {
+    private String titulo;
+    private String autor;
+    private int anhoEdicion;
+    
+    public Obra(){
+    
+    }
+    
+    public void setTitulo(String titu){
+    
+    this.titulo = titu;
+    }
+    
+    public String getTitulo(){
+    
+    return this.titulo;
+    }
+    public void setAutor(String auto){
+    
+    this.autor = auto;
+    }
+    
+    public String getAutor(){
+    
+    return this.autor;
+    }
+    
+    public void setAnhoEdicion(int anho){
+    
+    this.anhoEdicion = anho;
+    }
+    
+    public int getAnhoEdicion(){
+    
+    return this.anhoEdicion;
+    }
     
 }
