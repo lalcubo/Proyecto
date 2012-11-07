257ccb5d8d42d8777d7a24e10d6d2cc64d87d83a
Armando Moreno <amoreno@unet.edu.ve>
2012-11-07 16:57:16 -0430

    clase disco
    creancion de la clase con algun metodo

---
+++ src/practica/Disco.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica;
+
+/**
+ *
+ * @author Armando
+ */
+public class Disco extends Obra{
+    private String discografia;
+    private int nCanciones;
+    //////----constructor sin parametros-----
+    public Disco()
+    {
+        
+    }
+  //------ metodo para retornar la discografia
+  public String getDiscografia()
+  {
+     return discografia;
+  }
+  
+    
+}
