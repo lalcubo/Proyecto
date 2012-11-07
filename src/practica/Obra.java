141f01b870331be99e44c07dbd3f704c2522667a
Juan Ramirez <jramirezg@unet.edu.ve>
2012-11-07 15:10:54 -0430

    Modificacion Obra
    Creacion de Constructor con parametros

--- src/practica/Obra.java
+++ src/practica/Obra.java
@@ -9,38 +9,54 @@ package practica;
  * @author Juan
  */
 public class Obra {
-    private String titulo;
-    private String autor;
-    private int anhoEdicion;
+    private String titulo; //Título de la obra
+    private Artista autor; //Artista autor de la obra
+    private int anhoEdicion; //Año de edición de la obra
     
+    /*Constructor con parametros*/
+    public Obra(String titu,Artista auto,int anho){
+    
+        this.titulo = titu;
+        this.autor = auto;
+        this.anhoEdicion = anho;
+    }
+    
+    /*Constructor sin parametros*/
     public Obra(){
     
     }
     
+    /*Asigna el titulo de la obra*/
     public void setTitulo(String titu){
     
     this.titulo = titu;
     }
     
+    /*Obtiene o retorna el titulo de la obra*/
     public String getTitulo(){
     
     return this.titulo;
     }
-    public void setAutor(String auto){
+    
+     /*Asigna el artista autor de la obra*/
+    public void setAutor(Artista auto){
     
     this.autor = auto;
     }
     
-    public String getAutor(){
+    /*Obtiene o retorna el artista autor de la obra*/
+    public Artista getAutor(){
     
     return this.autor;
     }
     
+     /*Asigna el año de edición de la obra*/
     public void setAnhoEdicion(int anho){
     
     this.anhoEdicion = anho;
     }
     
+    /*Obtiene o retorna el año de edición de la obra*/
     public int getAnhoEdicion(){
     
     return this.anhoEdicion;
