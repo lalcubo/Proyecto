972bd2bc783048a685bcaae1301087859a8fb110
Luis Sanchez <asanchez@unet.edu.ve>
2012-11-11 15:24:38 -0430

    creacion metodos
    se creo los otros metodos de la clase pelicula

--- src/practica/Pelicula.java
+++ src/practica/Pelicula.java
@@ -3,11 +3,40 @@
  * and open the template in the editor.
  */
 package practica;
-
+import java.io.*;
+import java.util.*;
 /**
  *
  * @author Luis
  */
-public class Pelicula {
+public class Pelicula extends Obra{
+    private String productora;
+    private Artista interpretes;
+    InputStreamReader lector = new InputStreamReader(System.in);
+    BufferedReader leer = new BufferedReader(lector);
+    String opciones;
+    //////----constructor sin parametros-----
+    public Pelicula()
+    {
+        
+    }
+  /*Obtiene o retorna el artista autor de la pelicula*/
+    public Artista getInterpretes(){
     
+    return this.interpretes;
+    }
+     /*Asigna el artista autor de la pelicula*/
+    public void seInterpretes(Artista artis){
+    
+    this.interpretes = artis;
+    } 
+      //------ metodo para retornar la productora
+    public String getProductora()
+     {
+     return productora;
+    }
+    public void setProductora(String prod)
+    {
+        productora = prod;
+    }
 }
